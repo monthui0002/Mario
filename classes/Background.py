1c8050e0a479c355beb71ba43d73cffcebbd1800
monthui0002 <nguyenconghau0002@gmail.com>
2020-12-28 20:12:31 +0700

    sua file Tileset.json, them color cho background, sua file map.json

--- classes/Background.py
+++ classes/Background.py
@@ -13,49 +13,90 @@ tiles = Tile().tiles
 pygame.display.set_caption('MARIO')
 
 
+def add_map(urlListMap):
+    res = [0]
+    map_size = [(0, 0)]  # map 0 deo co
+    color = [[0,0,0]]
+    for url in urlListMap:
+        with open(url) as jsonData:
+            dic = {}
+            map = json.load(jsonData)
+            map_size.append((map['lengthx'] * tile_size * scale, map['lengthy'] * tile_size * scale))
+            color.append(map["color"])
+            list_object = map['object']
+            for object in list_object:
+                if object == "wall":
+                    dic[object] = {}
+                    for wall_name in list_object[object]:
+                        dic[object][wall_name] = list_object[object][wall_name]  # {"wall":{"wall_name":[pos]}}
+                elif object == "animation":
+                    dic[object] = list_object[object]  # {"animation:[[pos1][pos2]...]}
+                elif object == "background":
+                    dic[object] = {}
+                    for wall_name in list_object[object]:
+                        dic[object][wall_name] = list_object[object][
+                            wall_name]  # {"background":{"background_name":[pos]}}
+                res.append(dic)
+    return res, map_size,color  # dic{"name_tiles" : [position in map]}
+
+
 class Background:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.map, self.map_size = self.add_map([
+        self.map, self.map_size,self.color = add_map([
             "./levels/map1_1.json",
         ])
         self.index = 1  # defaul map = 1
         self.wall = self.load_wall()
+        self.background = self.load_background()
         self.coin = self.load_coin() # [[pos1],[pos2],..]
 
     def update(self, screen, player):
         self.check_camera(player)
         self.update_wall(screen)
+        self.update_background(screen)
         self.update_coin(tiles,screen)
 
-    def add_map(self, urlListMap):
-        dic = {}
-        map_size = [(0, 0)]  # map 0 deo co
-        for url in urlListMap:
-            with open(url) as jsonData:
-                map = json.load(jsonData)
-                map_size.append((map['lengthx']*tile_size*scale, map['lengthy']*tile_size*scale))
-                list_object = map['object']
-                for object in list_object:
-                    if object == "wall":
-                        dic[object] = {}
-                        for wall_name in list_object[object]:
-                            dic[object][wall_name] = list_object[object][wall_name]  # {"wall":{"wall_name":[pos]}}
-                    elif object == "animation":
-                        dic[object] = list_object[object]  # {"animation:[[pos1][pos2]...]}
-        return dic, map_size  # dic{"name_tiles" : [position in map]}
-
     def load_wall(self):
-        print(tiles)
-        print(self.map)
         load_wall = {}
-        # img = pygame.image.load("./img/Tileset.png")
-        for i in self.map["wall"]:
-            img = pygame.image.load(tiles[i][0])
-            img = img.subsurface(tiles[i][1], tiles[i][2], tiles[i][3][0], tiles[i][3][1])
-            load_wall[i] = pygame.transform.scale(img, (tiles[i][3][0] * scale, tiles[i][3][1] * scale))
+        if "wall" in self.map[self.index]:
+            # img = pygame.image.load("./img/Tileset.png")
+            for i in self.map[self.index]["wall"]:
+                img = pygame.image.load(tiles[i][0])
+                img = img.subsurface(tiles[i][1], tiles[i][2], tiles[i][3][0], tiles[i][3][1])
+                load_wall[i] = pygame.transform.scale(img, (tiles[i][3][0] * scale, tiles[i][3][1] * scale))
         return load_wall  # dictionary{"name_tiles" : img}
+
+    def load_background(self,):
+        load_background = {}
+        if "background" in self.map[self.index]:
+            for i in self.map[self.index]["background"]:
+                img = pygame.image.load(tiles[i][0])
+                img = img.subsurface(tiles[i][1], tiles[i][2], tiles[i][3][0], tiles[i][3][1])
+                load_background[i] = pygame.transform.scale(img, (tiles[i][3][0] * scale, tiles[i][3][1] * scale))
+        return load_background  # dictionary{"name_tiles" : img}
+
+    def load_coin(self):
+        coin = []
+        if "animation" in self.map[self.index]:
+            for pos in self.map[self.index]["animation"]:
+                coin.append(Coin(pos[0]*tile_size, pos[1]*tile_size))
+        return coin
+
+    def update_coin(self,tiles,screen):
+        for coin in self.coin:
+            coin.update(self.x,self.y,tiles,screen)
+
+    def update_wall(self,screen):
+        for tiles_name in self.map[self.index]["wall"]:
+            for i in self.map[self.index]["wall"][tiles_name]:
+                screen.blit(self.wall[tiles_name], (i[0] * tile_size * scale + self.x, i[1] * tile_size * scale + self.y))
+
+    def update_background(self,screen):
+        for tiles_name in self.map[self.index]["background"]:
+            for i in self.map[self.index]["background"][tiles_name]:
+                screen.blit(self.background[tiles_name], (i[0] * tile_size * scale + self.x, i[1] * tile_size * scale + self.y))
 
     def check_camera(self, player):
         x_camera = player.x - (window_size[0] - tile_size * scale) / 2
@@ -65,15 +106,3 @@ class Background:
             x_camera = self.map_size[self.index][0] - window_size[0]
         self.x = -x_camera
 
-    def load_coin(self):
-        coin = []
-        for pos in self.map["animation"]:
-            coin.append(Coin(pos[0]*tile_size, pos[1]*tile_size))
-        return coin
-    def update_coin(self,tiles,screen):
-        for coin in self.coin:
-            coin.update(self.x,self.y,tiles,screen)
-    def update_wall(self,screen):
-        for tiles_name in self.map["wall"]:
-            for i in self.map["wall"][tiles_name]:
-                screen.blit(self.wall[tiles_name], (i[0] * tile_size * scale + self.x, i[1] * tile_size * scale + self.y))
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ fpsClock = pygame.time.Clock()
 window_size = (25 * tile_size * scale, 14 * tile_size * scale)  # 25*14
 
 background = Background(0, 0)
-
+# "color": "rgb(135,206,250)",
 display_surface = pygame.display.set_mode(window_size)
 bg = pygame.transform.scale(display_surface, background.map_size[1])
 pygame.display.set_caption('MARIO')
@@ -19,6 +19,7 @@ mario = Mario(0, 0, Mario.DIRECTION_RIGHT, 1, Mario.IN_AIR, display_surface)
 
 while not mario.pause:
     display_surface = pygame.display.set_mode(window_size)  # (0,191,255)
+    display_surface.fill(background.color[background.index])
     background.update(display_surface, mario)
     mario.update(background)
     pygame.display.update()
